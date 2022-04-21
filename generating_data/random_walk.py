28ba7507e549f438f66b05ea113a6420c2a0aae0
Mike Lucent <mike.j.lucent@gmail.com>
2022-04-20 19:05:45 -0700

    adding visuals and refactored randomwalk code

--- generating_data/random_walk.py
+++ generating_data/random_walk.py
@@ -9,20 +9,23 @@ class RandomWalk:
         self.y_values = [0]
 
     #method to fill up values with steps
+
+    def get_step(self):
+
+            #choice and direction and distance, multiply to get step value
+            direction = random.choice([-1, 1])
+            distance = random.choice([0, 1, 2, 3, 4])
+            step = direction * distance 
+
+            return step
+
     def fill_walk(self):
 
         # while number of values is less than points keep going
         while len(self.x_values) < self.num_points:
 
-            #choice and direction and distance, multiply to get step value
-            x_direction = random.choice([-1, 1])
-            x_distance = random.choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance 
-
-            #choice and direction and distance, multiply to get step value
-            y_direction = random.choice([-1,1])
-            y_distance = random.choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance 
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             #if step value is 0 continue 
             if x_step ==0 and y_step ==0:
--- generating_data/rw_visual.py
+++ generating_data/rw_visual.py
@@ -3,15 +3,29 @@ import matplotlib.pyplot as plt
 
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(num_points=10000)
     rw.fill_walk()
 
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize=(15, 9))
+    ax.scatter(rw.x_values, rw.y_values,c = range(rw.num_points), cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    #emphasize the first and last point
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    #remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
 
-    keep_plotting = input("Plot again? Enter Y or N: ")
-    if keep_plotting.lower() == 'n':
-        break
+    while True:
+        keep_plotting = input("Plot again? Enter Y or N: ")
+        if keep_plotting.lower() != 'n' or 'y':
+            pass
+        if keep_plotting.lower() == 'y':
+            break       
+        if keep_plotting.lower() == 'n':
+            quit()
